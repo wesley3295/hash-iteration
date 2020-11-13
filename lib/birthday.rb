24a18794a0c380922307224cd2cd6d8617252d49
Wesley <wspencer3295@yahoo.com>
2020-11-13 12:26:44 -0700

    commit tree 6c83ce6bfe69e01cc8ec223d520a32dc28e1c210

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,8 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each {|a, b| puts "Happy Birthday #{a}! You are now #{b} years old!" }
+  birthday_kids
 end
 
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,13 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+
+ passengers.each do |suite, name|
+ if name.start_with?("A") && suite[0]
+  
+ end
+ return "#{name}"
 end
+end
+
+
